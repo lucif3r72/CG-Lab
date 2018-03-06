806067fb3ad3f339db6e734955a8fb477379b0f4
Nadir Sheikh <lucif3r721@gmail.com>
2018-03-06 11:57:10 +0530

    Add files via upload

---
+++ adv_motion.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <graphics.h>
+
+int main(){
+	int gd = DETECT, gm;
+	int t_fact, col, tx = 20;
+	
+	initgraph(&gd, &gm, NULL);
+	for(t_fact = 0; t_fact < 121; t_fact++){
+			
+			//carbody
+			
+			for(col = 1; col < 30 ; col++){
+				setcolor(col+1);
+				line(0 + tx, 320,-320 + tx,320);
+				setcolor(col+1);                     
+				line(0 + tx, 320,0 + tx,270);
+				setcolor(col+1);
+				line(-320 +tx,320,-320 + tx,270);
+				setcolor(col+1);
+				line(0 +tx,270,-320 + tx,270);
+			}
+            
+            //windows
+            
+            for(col = 1; col < 30 ; col++){
+				setcolor(col);
+				line(-120 + tx, 220, -50 + tx, 270);		//front
+				setcolor(col);
+				line(-220 + tx, 220, -120 + tx, 220);		//top
+				setcolor(col);
+				line(-250 + tx, 270, -220 + tx, 220);		//back
+				setcolor(col);
+				line(-220 + tx, 270, -220 + tx, 220);		//backwindow
+				setcolor(col);
+				line(-120 + tx, 220, -120 + tx, 270);		//frontwindow
+			}
+            
+            //frontwheel 			
+            
+            for(col = 1; col < 30 ; col++){
+				setcolor(col+2)
+				circle(-60 + tx, 350, 30);
+			}
+			
+			//backwheel 
+			for(col = 1; col < 30 ; col++){
+				setcolor(col+2) 						
+				circle(-260 + tx, 350, 30);
+			}
+			 						
+			tx = tx + 5;
+			delay(50);
+			cleardevice();
+		}
+		
+	closegraph();
+	return 0;
+}
