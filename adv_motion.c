63f8d2eea143b1aa2cc6a2be1d7645e44255286f
Nadir Sheikh <lucif3r721@gmail.com>
2018-03-09 00:25:28 +0530

    Add files via upload

--- adv_motion.c
+++ adv_motion.c
@@ -4,26 +4,29 @@
 int main(){
 	int gd = DETECT, gm;
 	int t_fact, col, tx = 20;
-	
+
 	initgraph(&gd, &gm, NULL);
 	for(t_fact = 0; t_fact < 121; t_fact++){
-			
+
 			//carbody
-			
-			for(col = 1; col < 30 ; col++){
+
+			for(col = 1; col < 16 ; col++){
 				setcolor(col+1);
 				line(0 + tx, 320,-320 + tx,320);
-				setcolor(col+1);                     
+				setcolor(col+1);
 				line(0 + tx, 320,0 + tx,270);
 				setcolor(col+1);
 				line(-320 +tx,320,-320 + tx,270);
 				setcolor(col+1);
 				line(0 +tx,270,-320 + tx,270);
+				if (col == 15)
+                    col = 2;
+				delay(1000);
 			}
-            
+
             //windows
-            
-            for(col = 1; col < 30 ; col++){
+
+            for(col = 1; col < 16 ; col++){
 				setcolor(col);
 				line(-120 + tx, 220, -50 + tx, 270);		//front
 				setcolor(col);
@@ -34,26 +37,35 @@ int main(){
 				line(-220 + tx, 270, -220 + tx, 220);		//backwindow
 				setcolor(col);
 				line(-120 + tx, 220, -120 + tx, 270);		//frontwindow
+				if (col == 15)
+                    col = 1;
+				delay(1000);
 			}
-            
-            //frontwheel 			
-            
-            for(col = 1; col < 30 ; col++){
-				setcolor(col+2)
+
+            //frontwheel
+
+            for(col = 1; col < 16 ; col++){
+				setcolor(col+2);
 				circle(-60 + tx, 350, 30);
+				if (col == 15)
+                    col = 1;
+				delay(1000);
 			}
-			
-			//backwheel 
-			for(col = 1; col < 30 ; col++){
-				setcolor(col+2) 						
+
+			//backwheel
+			for(col = 1; col < 16 ; col++){
+				setcolor(col+2);
 				circle(-260 + tx, 350, 30);
+				if (col == 15)
+                    col = 1;
+				delay(1000);
 			}
-			 						
+
 			tx = tx + 5;
 			delay(50);
 			cleardevice();
 		}
-		
+
 	closegraph();
 	return 0;
 }
